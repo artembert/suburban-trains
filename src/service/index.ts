a3fdbf489e918b452761506719ac01d67a154f3a
Artem Beresnev <artembert@gmail.com>
2020-04-24 21:39:14 +0300

    Install 'body-parser'

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,9 +1,11 @@
+import * as bodyParser from "body-parser";
 import * as chalk from "chalk";
 import * as express from "express";
 import { PORT } from "./base-config";
 import { getRouter } from "./routes/get";
 
 const app = express();
+app.use(bodyParser.json());
 app.use(`/get`, getRouter);
 
 app.listen(PORT, () => console.log(chalk.green(`Server listen on port ${PORT}`)));
