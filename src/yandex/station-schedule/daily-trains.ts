f3d8fea58ea6ad68afa8b2c4768d0689727f4839
Artem Beresnev <artembert@gmail.com>
2020-05-03 21:48:07 +0300

    Add script to get train directions

--- src/yandex/station-schedule/daily-trains.ts
+++ src/yandex/station-schedule/daily-trains.ts
@@ -6,6 +6,7 @@ import { Schedule, Station, toJson } from "./download-schedule";
 const commonDestStationsListFilePath = `../../../data/stations/dist/common-stations-list-${appStartDate}.json`;
 const dailyTrainsByStationsListFilePath = `../../../data/stations/dist/daily-trains-stations-list-${appStartDate}.json`;
 const activeStationsListFilePath = `../../../data/stations/dist/active-stations-list-${appStartDate}.json`;
+const directionsListFilePath = `../../../data/stations/dist/directions-list-${appStartDate}.json`;
 
 export async function dailyTrains(): Promise<void> {
   console.log(`START dailyTrains()`);
@@ -40,6 +41,24 @@ export async function activeStations(): Promise<void> {
     .map(station => station.code);
   await promises.writeFile(path.join(__dirname, activeStationsListFilePath), toJson(activeStationList));
   console.log(`Active stations list saved to ${activeStationsListFilePath}`);
+}
+
+export async function getDirections(): Promise<void> {
+  console.log(`START getDirections()`);
+  const stationsList: Station[] = JSON.parse(
+    (await promises.readFile(path.join(__dirname, commonDestStationsListFilePath))).toString(`utf8`),
+  ) as Station[];
+  const directionsList: [string, string][] = stationsList
+    .map(station =>
+      station.directions
+        .map(direction => direction.title)
+        .filter(direction => direction !== "прибытие" && direction !== "все направления")
+        .sort(),
+    )
+    .filter(station => !station.includes("на Москву")) as [string, string][];
+  const directionsMap = Array.from(new Map(directionsList));
+  await promises.writeFile(path.join(__dirname, directionsListFilePath), toJson(directionsMap));
+  console.log(`Directions list saved to ${directionsListFilePath}`);
 }
 
 function getDailyTrains(schedule: Schedule[]): number {
